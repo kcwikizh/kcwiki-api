23ae323834d38a4f5b0a50481a311bd201e0804f
grzhan <envy518@gmail.com>
2016-05-13 07:06:03 +0800

    feature(parse:start2): dump ship data from api_start2.json into local storage

--- app/Console/Commands/ParseStart2Command.php
+++ app/Console/Commands/ParseStart2Command.php
@@ -65,10 +65,15 @@ class ParseStart2 extends Command
         "wed_a" => "api_weda",
         "wed_b" => "api_wedb"
     ];
+    private $ship_common_keys = ['id', 'name', 'sort_no', 'stype', 'after_ship_id',
+        'filename', 'wiki_id', 'chinese_name', 'stype_name', 'stype_name_chinese'];
+    private $ship_type_chinese = ["海防舰", "驱逐舰", "轻巡洋舰", "重雷装巡洋舰", "重巡洋舰", "航空巡洋舰",
+        "轻空母", "战舰", "战舰", "航空战舰", "正规空母", "超弩级战舰", "潜水舰", "潜水空母", "补给舰",
+        "水上机母舰", "扬陆舰", "装甲空母", "工作舰", "潜水母舰", "练习巡洋舰", "补给舰"];
 
     public function handle()
     {
-        $this->info('Getting http://kcwikizh.github.io/kcdata/ship/all.json ...');
+        $this->info('Fetching http://kcwikizh.github.io/kcdata/ship/all.json ...');
         $kcdata = $this->sort(json_decode(file_get_contents('http://kcwikizh.github.io/kcdata/ship/all.json'), true), 'id');
         try {
             $start2data = json_decode(Storage::disk('local')->get('api_start2.json'), true);
@@ -86,7 +91,28 @@ class ParseStart2 extends Command
         $this->update($kcdata, $start2shipgraph, $this->ship_graph_map);
         $this->updateInKey($kcdata, $start2shipgraph, $this->ship_graph_detailed_map, 'graph');
         $kcdata = $this->toList($kcdata);
-        Storage::disk('local')->put('ship/all.json', json_encode($kcdata));
+        foreach ($kcdata as $i => $ship)
+            if (array_key_exists('stype', $ship) && $ship['stype'] > 0) {
+                $id = $ship['stype'];
+                echo "{$ship['id']} $id  {$start2shiptype[$id-1]['api_name']}  {$this->ship_type_chinese[$id-1]}\n";
+                $kcdata[$i]['stype_name'] = $start2shiptype[$id - 1]['api_name'];
+                $kcdata[$i]['stype_name_chinese'] = $this->ship_type_chinese[$id - 1];
+            }
+        Storage::disk('local')->put('ship/detailed/all.json', json_encode($kcdata));
+        $common_lists = [];
+        // extract common use data from the previous results
+        foreach ($kcdata as $i => $ship)
+            if (array_key_exists('id', $ship)) {
+                $id = $ship['id'];
+                Storage::disk('local')->put("ship/detailed/$id.json", json_encode($ship));
+                $common = [];
+                foreach($this->ship_common_keys as $j => $key)
+                    if (array_key_exists($key, $ship))
+                        $common[$key] = $ship[$key];
+                if (count($common) > 0) array_push($common_lists, $common);
+                Storage::disk('local')->put("ship/$id.json", json_encode($common));
+            }
+        Storage::disk('local')->put('ship/all.json', json_encode($common_lists));
         $this->info('Completed.');
     }
 

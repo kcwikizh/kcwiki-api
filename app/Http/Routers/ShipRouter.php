1aceae3e468105d482625506feb54e1e38adc49d
grzhan <envy518@gmail.com>
2016-05-13 12:02:12 +0800

    feature(ship router): support more interfaces

--- app/Http/Routers/ShipRouter.php
+++ app/Http/Routers/ShipRouter.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Storage;
+use App\SubtitleCache;
 
 $app->get('/ships', ['middleware' => 'cache', function() {
     $raw = Storage::disk('local')->get('ship/all.json');
@@ -20,3 +21,44 @@ $app->get('/ship/detail/{id:\d{1,3}}', ['middleware' => 'cache', function($id) {
     $raw = Storage::disk('local')->get("ship/detailed/$id.json");
     return response($raw);
 }]);
+
+$app->get('/ships/filename', ['middleware' => 'cache', function() {
+    $raw = Storage::disk('local')->get('ship/filename/all.json');
+    return response($raw);
+}]);
+
+$app->get('/ship/filename/{id:\d{1,3}}', ['middleware' => 'cache', function($id) {
+    $raw = Storage::disk('local')->get("ship/filename/$id.json");
+    return response($raw);
+}]);
+
+$app->get('/ships/stats', ['middleware' => 'cache', function() {
+    $raw = Storage::disk('local')->get('ship/stats/all.json');
+    return response($raw);
+}]);
+
+$app->get('/ship/stats/{id:\d{1,3}}', ['middleware' => 'cache', function($id) {
+    $raw = Storage::disk('local')->get("ship/stats/$id.json");
+    return response($raw);
+}]);
+
+$app->get('/ship/detail/{name:.{1,50}}', ['middleware' => 'cache', function($name) {
+    $ships = json_decode(SubtitleCache::remember('ships/detail', function() { return Storage::disk('local')->get('ship/detailed/all.json');}), true);
+    $name = urldecode($name);
+    foreach($ships as $i => $ship)
+        if ($ship['name'] === $name || $ship['chinese_name'] === $name) {
+            return response()->json($ship);
+        }
+    return response()->json(['result'=>'error', 'reason' => 'ship not found']);
+}]);
+
+$app->get('/ship/{name:.{1,50}}', ['middleware' => 'cache', function($name) {
+    $ships = json_decode(SubtitleCache::remember('ships', function() { return Storage::disk('local')->get('ship/all.json');}), true);
+    $name = urldecode($name);
+    foreach($ships as $i => $ship)
+        if ($ship['name'] === $name || $ship['chinese_name'] === $name) {
+            return response()->json($ship);
+        }
+    return response()->json(['result'=>'error', 'reason' => 'ship not found']);
+}]);
+

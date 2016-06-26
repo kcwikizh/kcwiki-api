a7a618e35d8d7657cb6fa70d0dc5a3eae0c69212
grzhan <envy518@gmail.com>
2016-06-27 05:09:33 +0800

    feature: update index page content

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -5,15 +5,14 @@
     <title>Kcwiki 新闻API</title>
     <link rel="stylesheet" href="/css/semantic.min.css">
     <style>
-        .ui.blue.segment {
-            margin-top: 100px;
-        }
+        .ui.blue.segment { margin-top: 100px; padding-bottom: 0px; }
+        body { background-color: #E8ECF2; }
     </style>
 </head>
 <body>
     <div class="ui three column centered stackable grid">
         <div class="column">
-            <div class="ui blue segment">
+            <div class="ui attached blue segment">
                 <h2 class="ui centered dividing header">
                     舰娘百科API
                     <div class="sub header">公开的API集合，{var}表示可变参数</div>
@@ -21,37 +20,39 @@
                 <div class="ui relaxed divided list">
                     <div class="item"> <i class="large linkify middle aligned icon"></i>
                         <div class="content">
-                            <a class="header" href="http://t.kcwiki.moe/?json=1&count=15">舰娘官方推特转发</a>
-                            <div class="description">http://t.kcwiki.moe/?json=1&amp;count={num}</div>
+                            <a class="header" href="//api.kcwiki.moe/tweet/15">舰娘官方推特转发</a>
+                            <div class="description">api.kcwiki.moe/tweet/{num}</div>
                         </div>
                     </div>
                     <div class="item"> <i class="large linkify middle aligned icon"></i>
                         <div class="content">
-                            <a class="header" href="http://static.kcwiki.moe/KanColleStaffAvatar.png">舰娘官方推特头像</a>
-                            <div class="description">http://static.kcwiki.moe/KanColleStaffAvatar.png</div>
+                            <a class="header" href="//api.kcwiki.moe/avatar/latest">舰娘官方推特头像</a>
+                            <div class="description">api.kcwiki.moe/avatar/latest</div>
 
                         </div>
                     </div>
                     <div class="item"> <i class="large linkify middle aligned icon"></i>
                         <div class="content">
-                            <a class="header" href="http://static.kcwiki.moe/KanColleStaffAvatar.png">舰娘抢号时间与服务器维护时间</a>
-                            <div class="description">http://zh.kcwiki.moe/api.php?action=parse&amp;text=@{{更新内容}}&amp;format=json</div>
+                            <a class="header" href="//api.kcwiki.moe/ship/1">舰娘基本信息（例：睦月）</a>
+                            <div class="description">api.kcwiki.moe/ship/{ID或舰娘名称}</div>
                         </div>
                     </div>
                     <div class="item"> <i class="large linkify middle aligned icon"></i>
                         <div class="content">
-                            <a class="header" href="http://db.kcwiki.moe/wiki/enemy/23.json">敌舰配置数据</a>
-                            <div class="description">http://db.kcwiki.moe/wiki/enemy/{num}.json</div>
+                            <a class="header" href="//api.kcwiki.moe/slotitem/1">装备基本信息</a>
+                            <div class="description">api.kcwiki.moe/slotitem/{ID}</div>
                         </div>
                     </div>
                     <div class="item"> <i class="large linkify middle aligned icon"></i>
                         <div class="content">
-                            <a class="header" href="http://api.kcwiki.moe/news">舰娘百科移动端新闻API</a>
-                            <div class="description">http://api.kcwiki.moe/news</div>
+                            <a class="header" href="http://api.kcwiki.moe/start2">Start2 数据</a>
+                            <div class="description">api.kcwiki.moe/start2</div>
                         </div>
                     </div>
                 </div>
-                
+            </div>
+            <div class="ui bottom attached center aligned segment">
+                <a href="https://github.com/kcwikizh/kcwiki-api/wiki">详情参考文档</a>
             </div>
         </div>
     </div>
